e1c4ce88576bfa2f0ad0bdac64f9e1a7ac3c58dc
Joonas <joonas.lassila95@gmail.com>
2019-12-06 02:48:21 +0200

    upload and delete

--- korppi.js
+++ korppi.js
@@ -2,15 +2,18 @@ var Discord = require('discord.js');
 var logger = require('winston');
 var opus = require('node-opus');
 var opusScript = require('opusscript');
-var {prefix, token} = require('./auth.json');
+var { prefix, token } = require('./auth.json');
 var fs = require('fs');
 var path = require('path');
 var os = require('os');
+var https = require('https');
+
+var textChannel = null;
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
-    colorize: true
+	colorize: true
 });
 
 logger.level = 'debug';
@@ -20,33 +23,41 @@ var client = new Discord.Client();
 var isReady = true;
 
 client.on('ready', async evnt => {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(client.username + ' - (' + client.id + ')');
-    logger.info('Prefix: ' + prefix);
+	logger.info('Connected');
+	logger.info('Logged in as: ');
+	logger.info(client.username + ' - (' + client.id + ')');
+	logger.info('Prefix: ' + prefix);
 });
 
-client.on('message', async message =>{
-	if(message.author.bot) return;
-	if(message.content.startsWith(prefix)) {
-		
+client.on('message', async message => {
+	textChannel = message.channel;
+	if (message.author.bot) return;
+	if (message.content.startsWith(prefix)) {
+
 		var voiceChannel = message.member.voice.channel;
-		if(!voiceChannel) {
-			message.channel.send("Saatanan lurjus, ei se huutelu toimi, ellet ole kaikukammiossa :--()");
-		}
 
 		//logger.debug("VoiceChannel: " + voiceChannel);
 		var args = message.content.substring(1).split(' ');
 		var cmd = args[0];
 
-		if(cmd == "fuckoff") {
+		if (cmd == "fuckoff") {
 			voiceChannel.leave();
 			isReady = true;
-		} else if(cmd == "p\xE4ivit\xE4") {
-			updateSounds(message.channel);
-		} else if(cmd == "listaa") {
-			showSounds(message.channel);
-		} else if(isReady && voiceChannel) {
+		} else if (cmd == "p\xE4ivit\xE4") {
+			updateSounds();
+		} else if (cmd == "listaa") {
+			showSounds();
+		} else if (cmd == "upload") {
+			if (message.attachments.size == 0) {
+				message.channel.send("Hemmetin sokea lepakko, et antanut minulle tiedostoa ladattavaksi!");
+			} else {
+				message.attachments.forEach(upload);
+			}
+		} else if (cmd == "delete") {
+			deleteSound(args[1]);
+		} else if (!voiceChannel) {
+			message.channel.send("Saatanan lurjus, ei se huutelu toimi, ellet ole kaikukammiossa :--()");
+		} else if (isReady && voiceChannel) {
 			isReady = false;
 			play(voiceChannel, "Sounds/" + cmd + ".mp3");
 		}
@@ -55,7 +66,7 @@ client.on('message', async message =>{
 });
 
 function play(voiceChannel, sound) {
-	voiceChannel.join().then(connection =>{
+	voiceChannel.join().then(connection => {
 		const dispatcher = connection.play(sound);
 		dispatcher
 			.on("end", end => {
@@ -64,42 +75,102 @@ function play(voiceChannel, sound) {
 	}).catch(err => logger.error(err.message));
 }
 
-function showSounds(textChannel) {
-	fs.readFile('sounds.txt', function(err, data) {
+function showSounds() {
+	readSoundsFile(function (data) {
 		textChannel.send("Tarjolla olevat \xE4\xE4net tulevat t\xE4ss\xE4, valitkaa suosikkinne: \n\n" + data);
+	})
+}
+
+function readSoundsFile(callback) {
+	fs.readFile('sounds.txt', function (err, data) {
+		if (err) throw err;
+		callback(data);
 	});
 }
 
-
-client.login(token);
-
-
-function updateSounds(textChannel) {
-
-//update available sounds into sounds.txt
-var sounds = "";
-
-fs.readdir("Sounds", function(err, files) {
-	if(err) {
-		logger.error("Unable to scan directory: " + err.message);
+function upload(attachment, snowflake) {
+	logger.info("Attachment: " + attachment);
+	logger.info("Snowflake: " + snowflake);
+	var reqexpMP3 = /.*\.mp3/;
+	if (!reqexpMP3.test(attachment.name)) {
+		textChannel.send("Älä sinä kehveli koita syöttää minulle muuta kuin mp3-tiedostoja!!!");
 		return;
 	}
-
-	files.forEach(function(file) {
-		sounds += file.split(".")[0];
-		sounds += os.EOL;
+	var path = 'Sounds/' + attachment.name;
+	var file = fs.createWriteStream(path);
+	https.get(attachment.proxyURL, function (res) {
+		res.pipe(file);
 	});
+	file.on('finish', function () {
+		updateSounds(function () {
+			readSoundsFile(function (data) {
+				var MP3ext = /\.mp3$/;
+				if (data.toString('utf8').includes(attachment.name.replace(MP3ext, ""))) {
+					textChannel.send("Paskainen ääniklippisi on ladattu onnistuneesti, saatanan kurttumuna....");
+				} else {
+					logger.info(attachment.name.replace(MP3ext, ""));
+					logger.info(data.toString('utf8'));
+				}
+			});
+		});
 
-	//logger.info("Sounds: " + sounds);
+	})
+}
 
-	fs.writeFile('sounds.txt', sounds, function(err) {
-		if(err) {
-			logger.error("Unable to write file: " + err.message);
+function deleteSound(soundname) {
+	readSoundsFile(function (data) {
+		if(soundname === undefined) {
+			textChannel.send("Pelleiletkö kanssani? Kerroppas mursu nyt ihan selkeästi, että mitä haluat poistaa.");
 			return;
-		}
+		} else if (!data.toString('utf8').includes(soundname)) {
+			textChannel.send("Taliaivo!!! Eihän minulla edes ole moista klippiä, perkele!!!");
+			return;
+		} 
+		fs.unlink("Sounds/" + soundname + ".mp3", function (err) {
+			if (err) {
+				textChannel.send("Jokin meni vikaan ääntä poistaessa, voihan saatanan saatana!");
+				logger.error(err.message);
+				return;
+			}
+			textChannel.send("Ääni nimeltä " + soundname + " poistettu. Se olikin aika kuraa...");
+			updateSounds();
+		});
 	});
-});
-
-textChannel.send("\xC4\xE4nilista p\xE4ivitetty! Sus meni niinkuin Sas....");
 
 }
+
+function updateSounds(callback = null) {
+
+	//update available sounds into sounds.txt
+	var sounds = "";
+
+	fs.readdir("Sounds", function (err, files) {
+		if (err) {
+			logger.error("Unable to scan directory: " + err.message);
+			return;
+		}
+
+		files.forEach(function (file) {
+			sounds += file.split(".")[0];
+			sounds += os.EOL;
+		});
+
+		//logger.info("Sounds: " + sounds);
+
+		fs.writeFile('sounds.txt', sounds, function (err) {
+			if (err) {
+				logger.error("Unable to write file: " + err.message);
+				return;
+			}
+			textChannel.send("\xC4\xE4nilista p\xE4ivitetty! Sus meni niinkuin Sas....");
+
+			if (!(callback == null)) {
+				callback();
+			}
+		});
+	});
+
+
+}
+
+client.login(token);
